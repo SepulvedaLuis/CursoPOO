d432b72f3f3614cf35f31470871776b2bd52497a
Luis Sepulveda <sepulvedaluis022@gmail.com>
2021-07-01 14:01:49 -0500

    Reducimos nuestro código para crear objetos, gracias a los constructores

--- Java/Main.java
+++ Java/Main.java
@@ -3,17 +3,21 @@ package Java;
 class Main {
     public static void main(String[] args) {
         // System.out.println("Hola Mundo");
-        Car car = new Car(); // Por defecto toda clase genera el método contructor de forma automática
-        car.license = "AMQ123"; // Asignamos datos
-        car.driver = "Andres Herrera";
-        car.passegender = 4;
+        Car car = new Car("AMQ123", new Account("Andres Herrera", "CDB1235"), 4);
+        // Lo que ganamos definiendo el método contructor fue que reducir líneas de
+        // código y podemos
+        // encontrar que al definir nuestro objeto car le debemos pasar nuestros datos
+        // definidos en
+        // el contrusctor y como tenemos un atributo heredado lo que hacemos es crear un
+        // objeo de Account
+        // y llamarlo en Car
         car.printDataCar(); // Con este método imprimimos los datos de nuestro objeto car
 
-        Car car2 = new Car();
-        car2.license = "QWE567";
-        car2.driver = "Luis Gonzalez";
-        car2.passegender = 3;
-        car2.printDataCar(); // Con este método imprimimos los datos del objeto car2
+        // Car car2 = new Car();
+        // car2.license = "QWE567";
+        // car2.driver = "Luis Gonzalez";
+        // car2.passegender = 3;
+        // car2.printDataCar(); // Con este método imprimimos los datos del objeto car2
     }
 }
 // Nuestro primer ejemplo de clases en Java en la cual imprimimos un mensaje
