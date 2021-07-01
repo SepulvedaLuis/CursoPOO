d063addd7ae86fb3615fb3d52986366cb4104c5a
Luis Sepulveda <sepulvedaluis022@gmail.com>
2021-07-01 14:00:08 -0500

    Creamos nuestro método contructor

--- Java/Account.java
+++ Java/Account.java
@@ -2,9 +2,16 @@ package Java;
 
 class Account {
     Integer id;
-    String nombre;
+    String name;
     String document;
     String email;
     String password;
+
+    // método contructor
+    public Account(String name, String document) {
+        this.name = name;
+        this.document = document;
+        // siempre usamos la palabra reservada this para el método contructor
+    }
 }
 // Creamos una clase con valores enteros y string
