528aba2477d9aef9d987a7806ab5ac86e9661277
Luis Sepulveda <sepulvedaluis022@gmail.com>
2021-07-01 13:23:45 -0500

    Creamos nuestros primeros objetos

--- Java/Main.java
+++ Java/Main.java
@@ -2,7 +2,18 @@ package Java;
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        // System.out.println("Hola Mundo");
+        Car car = new Car(); // Por defecto toda clase genera el método contructor de forma automática
+        car.license = "AMQ123"; // Asignamos datos
+        car.driver = "Andres Herrera";
+        car.passegender = 4;
+        car.printDataCar(); // Con este método imprimimos los datos de nuestro objeto car
+
+        Car car2 = new Car();
+        car2.license = "QWE567";
+        car2.driver = "Luis Gonzalez";
+        car2.passegender = 3;
+        car2.printDataCar(); // Con este método imprimimos los datos del objeto car2
     }
 }
 // Nuestro primer ejemplo de clases en Java en la cual imprimimos un mensaje
