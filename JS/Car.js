ec138e9905306d88970ab7b87f80d44d6f6a7b44
Luis Sepulveda <sepulvedaluis022@gmail.com>
2021-07-01 14:17:16 -0500

    Agregamos nuestro atributos para el método contructor y método para imprimir

--- JS/Car.js
+++ JS/Car.js
@@ -1,8 +1,14 @@
-function Car() {
-    this.id;
-    this.license;
-    this.driver;
-    this.passenger;
-    
+function Car(license, driver, passenger) { //Indicandolo los atributos que deseamos ya generamos nuestro
+    this.id;                      ///método contrusctor y debemos instanciar cada valor
+    this.license = license;
+    this.driver = driver;
+    this.passenger = passenger;
+}
+//Creamos una función o método en nuestra clase
+Car.prototype.printDataCar = function (){
+    console.log(this.driver)
+    console.log(this.driver.name)
+    console.log(this.driver.document)
+    console.log(this.passenger)
 }
 //Clase creada con sus atributos
