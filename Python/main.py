5ad56c194f103bd6e799c6c12a89d076f0dafbc5
Luis Sepulveda <sepulvedaluis022@gmail.com>
2021-07-01 13:35:16 -0500

    Creamos nuestros primeros objetos

--- Python/main.py
+++ Python/main.py
@@ -1,4 +1,17 @@
+from car import Car #Importamos la clase que vamos a usar
+
 if __name__ == "__main__":
-    print("Hello world")
+    #print("Hello world")
+    car = Car() #Generamos nuestro primer objeto con la clase Car
+    car.license = "AMS234"
+    car.driver = "Andres Herrera" #Creamos los atributos de nuestra clase
+    car.passegender = 3
+    print(vars(car))  #Imprimimos los valores de nuestro objeto, se imprimi en formato .Json
+
+    car2 =  Car()  #Creamos un nuevo objeto
+    car2.license = "QTY786"
+    car2.driver = "Marta Diaz"
+    car2.passegender = 4
+    print(vars(car2))
 
 #Primer ejemplo de clase y enviar un mensaje
