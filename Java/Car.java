d3fafdf942e6c29a600cb25526faab43e7c555cb
Luis Sepulveda <sepulvedaluis022@gmail.com>
2021-07-01 14:01:01 -0500

    Creamos nuestro método constructor y heredamos un atributo de la clase Account

--- Java/Car.java
+++ Java/Car.java
@@ -3,12 +3,22 @@ package Java;
 class Car {
     Integer id;
     String license;
-    String driver;
+    Account driver;
     Integer passegender;
 
-    void printDataCar() { // Creamos un método para imprimir los datos creados
-        System.out.println("License: " + license + "\n" + "Driver: " + driver + "\n" + "Passenger: " + passegender);
+    // Vamos a crear nuestro método constructor en java
+    public Car(String license, Account driver, Integer passegender) {
+        this.license = license;
+        this.driver = driver;
+        this.passegender = passegender;
+        // Debemos siempre usar la palabra reservada this.
     }
+
+    void printDataCar() { // Creamos un método para imprimir los datos creados
+        System.out.println(
+                "License: " + license + "\n" + "Name Driver: " + driver.name + "\n" + "Passenger: " + passegender);
+    } // para pasar el nombre solo debemos entonces que llamar el dato que necesitamos
+      // de la clase
+      // Account y debemos hacerlo con el atributoClaseHija.atributoClasePadre
+
 }
-// Clase car con ID tipo entero y el número de pasageros, adicional dos strings
-// para license y driver
