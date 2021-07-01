a58aa25598f5b1a5a82b76922fa35ab41af15a33
Luis Sepulveda <sepulvedaluis022@gmail.com>
2021-07-01 13:23:20 -0500

    Agregamos un método para imprimir en la clase

--- Java/Car.java
+++ Java/Car.java
@@ -5,6 +5,10 @@ class Car {
     String license;
     String driver;
     Integer passegender;
+
+    void printDataCar() { // Creamos un método para imprimir los datos creados
+        System.out.println("License: " + license + "\n" + "Driver: " + driver + "\n" + "Passenger: " + passegender);
+    }
 }
 // Clase car con ID tipo entero y el número de pasageros, adicional dos strings
 // para license y driver
